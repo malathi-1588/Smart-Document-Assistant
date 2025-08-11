a5445f0283b0df82c9b85987778f3e3f56044bd9
Malathi M <116137959+malathi-1588@users.noreply.github.com>
2025-08-11 20:02:31 +0530

    Add frontend project files (excluding node_modules)

---
+++ smart-doc-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import UploadForm from "./components/UploadForm";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Smart Document Assistant</h1>
+      <UploadForm />
+    </div>
+  );
+}
